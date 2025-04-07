8a6bf82b9d7d74f74099ead8a638b9dcba2252cf
sebik19 <ionut-sebastian.ignat@student.tuiasi.ro>
2025-04-07 17:42:30 +0300

    Repair scaling

--- src/PaooGame/Main.java
+++ src/PaooGame/Main.java
@@ -4,6 +4,7 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
+        System.setProperty("sun.java2d.uiScale", "1"); //repair scaling
         Dimension screenDimension = Toolkit.getDefaultToolkit().getScreenSize();
         System.out.println(screenDimension.width + " " + screenDimension.height);
         Game paooGame = new Game("PaooGame", screenDimension.width, screenDimension.height);
