ccb263b5f6b9d7fc7c356484501aeeb3175caad9
callmepillo <matei.iliescu@student.tuiasi.ro>
2025-04-10 15:08:14 +0300

    Implemented a primitive version of the level class that renders inside a window.

---
+++ src/PaooGame/Levels/Level.java
@@ -0,0 +1,35 @@
+package PaooGame.Levels;
+
+import PaooGame.Tiles.Tile;
+
+import java.awt.*;
+
+public class Level {
+    public static final int LEVEL_WIDTH = 10; //Sizes for the level
+    public static final int LEVEL_HEIGHT = 6; //Sizes for the level
+    public static int[][][] tileMap;
+    //this is a tile map that contains the rooms, the rows and the columns of tiles
+    //the coordinates correspond to [room][row][column]
+
+    public static Level testLevel = new TestLevel(); //static variable so we load the level at start
+
+    //Draw function for a level
+    public void Draw(Graphics2D g, int leftBound, int rightBound, int upperBound, int room) {
+        for(int i = 0; i < LEVEL_HEIGHT; ++i) {
+            for(int j = 0; j < LEVEL_WIDTH; ++j) {
+                if(tileMap[room][i][j] != 0) {
+                    if (leftBound + (j+1) * Tile.TILE_WIDTH <= rightBound)
+                        Tile.tiles[tileMap[room][i][j]].Draw(g, leftBound + j * Tile.TILE_WIDTH,  upperBound + i * Tile.TILE_HEIGHT);
+                    else
+                        break;
+                }
+            }
+        }
+        /*The draw functions iterates over each tile of a room and draws them if it is visible
+        * In this case, the function only searches if the end tile has space in the window
+        * (by checking if leftBound + (j+1)* Tile.TILE_WIDHT is smaller than rightBound
+        * This will need to be modified when we implement the camera and the player traversal rules
+        * For this reason, there is an overloaded function of Tile::Draw that also takes a width
+        * and height argument to display only slices of a tile*/
+    }
+}
