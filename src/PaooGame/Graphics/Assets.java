fe2a3355e3c23feda48dce72c05df60fc94f558f
sebik19 <ionut-sebastian.ignat@student.tuiasi.ro>
2025-04-11 09:11:11 +0300

    test

--- src/PaooGame/Graphics/Assets.java
+++ src/PaooGame/Graphics/Assets.java
@@ -39,21 +39,70 @@ public class Assets
         SpriteSheet sheet = new SpriteSheet(ImageLoader.LoadImage("/textures/firstCorrectSprite_resized.png"));
 
             /// Se obtin subimaginile corespunzatoare elementelor necesare.
-        grass = sheet.crop(0, 0);
-        soil = sheet.crop(1, 0);
-        water = sheet.crop(2, 0);
-        mountain = sheet.crop(3, 0);
-        townGrass = sheet.crop(0, 1);
-        townGrassDestroyed = sheet.crop(1, 1);
-        townSoil = sheet.crop(2, 1);
-        tree = sheet.crop(3, 1);
-        playerLeft = sheet.crop(0, 2);
-        playerRight = sheet.crop(1, 2);
-        rockUp = sheet.crop(2, 2);
-        rockDown = sheet.crop(3, 2);
-        rockLeft = sheet.crop(0, 3);
-        rockRight = sheet.crop(1, 3);
+//        grass = sheet.crop(0, 0);
+//        soil = sheet.crop(1, 0);
+//        water = sheet.crop(2, 0);
+//        mountain = sheet.crop(3, 0);
+//        townGrass = sheet.crop(0, 1);
+//        townGrassDestroyed = sheet.crop(1, 1);
+//        townSoil = sheet.crop(2, 1);
+//        tree = sheet.crop(3, 1);
+//        playerLeft = sheet.crop(0, 2);
+//        playerRight = sheet.crop(1, 2);
+//        rockUp = sheet.crop(2, 2);
+//        rockDown = sheet.crop(3, 2);
+//        rockLeft = sheet.crop(0, 3);
+//        rockRight = sheet.crop(1, 3);
+
         plant = sheet.crop(9,3);
-        floor = sheet.crop(1, 4);
+        floor_middle = sheet.crop(1, 4);
+
+
+        left_upper_door=sheet.crop(0,0);
+        left_midle_door=sheet.crop(0,1);
+        left_down_door=sheet.crop(0,2);
+
+
+        right_upper_door=sheet.crop(1,0);
+        right_midle_door=sheet.crop(1,1);
+        right_down_door=sheet.crop(1,2);
+
+        thin_half_wall_left_up=sheet.crop(2, 0);
+        thin_half_wall_left_middle=sheet.crop(2, 1);
+        thin_half_wall_left_down=sheet.crop(2, 2);
+
+        thin_half_wall_right_up=sheet.crop(3, 0);
+        thin_half_wall_right_middle=sheet.crop(3, 1);
+        thin_half_wall_right_down=sheet.crop(3, 2);
+
+
+        thick_half_wall_left_up=sheet.crop(4, 0);
+        thick_half_wall_left_middle=sheet.crop(4, 1);
+        thick_half_wall_left_down=sheet.crop(4, 2);
+
+
+        thin_window_up=sheet.crop(5,0);
+        thin_window_middle=sheet.crop(5,1);
+        thin_window_down=sheet.crop(5,2);
+
+
+        rounded_wall_up=sheet.crop(6,0);
+        rounded_wall_middle=sheet.crop(6,1);
+        rounded_wall_down=sheet.crop(6,2);
+
+        half_left_window=sheet.crop(7,0);
+        half_left_window=sheet.crop(7,1);
+        half_left_window=sheet.crop(7,2);
+
+        half_right_window=sheet.crop(8,0);
+        half_right_window=sheet.crop(8,1);
+        half_right_window=sheet.crop(8,2);
+
+        arcade_tree_up=sheet.crop(9,0);
+        arcade_tree_down=sheet.crop(9,1);
+
+
+
+
     }
 }
